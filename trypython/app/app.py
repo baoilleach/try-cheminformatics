713f2327dfd13ed99893a4d80a1b61bf55fe3002
fuzzyman <fuzzyman@ab4eaae6-8d89-11de-8988-25e8a898b690>
2009-08-29 14:48:02 +0000

    Navigation bar and new tutorial pages.
    git-svn-id: http://trypython.googlecode.com/svn/trunk@24 ab4eaae6-8d89-11de-8988-25e8a898b690

--- trypython/app/app.py
+++ trypython/app/app.py
@@ -14,16 +14,16 @@ from System.Windows.Markup import XamlReader
 
 Application.Current.LoadRootVisual(StackPanel(), "app.xaml")
 root = Application.Current.RootVisual
-combobox = root.comboBox
+topCombobox = root.topComboBox
+bottomCombobox = root.bottomComboBox
 
 def content_resized(sender, event):
     root.Width = width = max(Application.Current.Host.Content.ActualWidth - 25, 700)
     root.Height = height = max(Application.Current.Host.Content.ActualHeight - 25, 700)
 
     root.document.Width = int(width * 0.53)
-    root.document.Height = height - 90
+    root.container.Height = height - 100
     root.scroller.Width = int(width * 0.44)
-    root.scroller.Height = height - 90
 
 Application.Current.Host.Content.Resized += content_resized
 content_resized(None, None)
@@ -42,23 +42,41 @@ import console
 with open('list.txt') as handle:
     items = handle.readlines()
 
-for item in items:
-    boxitem = ComboBoxItem()
-    boxitem.Content = item
-    boxitem.Height = 25
-    combobox.Items.Add(boxitem)
+for combobox in topCombobox, bottomCombobox:
+    for item in items:
+        boxitem = ComboBoxItem()
+        boxitem.Content = item
+        boxitem.Height = 25
+        combobox.Items.Add(boxitem)
     
-def onChange(sender, event):
-    index = combobox.SelectedIndex
-    HtmlPage.Window.CurrentBookmark = 'page%s' % (index + 1)
+def onChangeTop(sender, event):
+    index = topCombobox.SelectedIndex
+    bottomCombobox.SelectionChanged -= onChangeBottom
+    bottomCombobox.SelectedIndex = index
+    bottomCombobox.SelectionChanged += onChangeBottom
+    changeDocument(index)
+    
+def onChangeBottom(sender, event):
+    index = bottomCombobox.SelectedIndex
+    topCombobox.SelectionChanged -= onChangeTop
+    topCombobox.SelectedIndex = index
+    topCombobox.SelectionChanged += onChangeTop
+    changeDocument(index)
+
+def changeDocument(index):
+    page = ''
+    if index > 0:
+        page = 'page%s' % (index + 1)
+    HtmlPage.Window.CurrentBookmark = page
     with open('docs/item%s.xaml' % (index+1)) as handle:
         xaml = handle.read()
     document = XamlReader.Load(xaml)
-    root.document.Content = document
-    root.document.ScrollToVerticalOffset(0)
+    root.document.Child.Content = document
+    root.document.Child.ScrollToVerticalOffset(0)
     console.focus_text_box(None, None)
 
-combobox.SelectionChanged += onChange
+topCombobox.SelectionChanged += onChangeTop
+bottomCombobox.SelectionChanged += onChangeBottom
 
 page = 0
 bookmark = HtmlPage.Window.CurrentBookmark.lower()
@@ -70,6 +88,27 @@ if bookmark.startswith('page'):
     else:
         page = min((page - 1), len(combobox.Items) - 1)
         page = max(page, 0)
+
+def first(sender, event):
+    topCombobox.SelectedIndex = 0
+def last(sender, event):
+    topCombobox.SelectedIndex = len(combobox.Items) - 1
+def next(sender, event):
+    current = combobox.SelectedIndex
+    topCombobox.SelectedIndex = min(current + 1, len(combobox.Items) - 1)
+def prev(sender, event):
+    current = combobox.SelectedIndex
+    topCombobox.SelectedIndex = max(current - 1, 0)
+    
+    
+root.topFirst.Click += first
+root.bottomFirst.Click += first
+root.topLast.Click += last
+root.bottomLast.Click += last
+root.topNext.Click += next
+root.bottomNext.Click += next
+root.topPrev.Click += prev
+root.bottomPrev.Click += prev
         
-combobox.SelectedIndex = page
+topCombobox.SelectedIndex = page
 console.focus_text_box(None, None)
